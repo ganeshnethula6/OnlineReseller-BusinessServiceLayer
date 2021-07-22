2e08c01ce9c97bd3e7e1c1c67e0fc3cadd04f599
Ganesh Nethula <ganeshnethula6@gmail.com>
2021-07-22 22:28:06 +0530

    ONRES-11: Create BSL Project for Online ReSeller Application

---
+++ src/main/java/com/onlinereseller/gui/BusinessServiceLayerApplication.java
@@ -0,0 +1,13 @@
+package com.onlinereseller.gui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessServiceLayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessServiceLayerApplication.class, args);
+	}
+
+}
